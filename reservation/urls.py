5d6e879794ae88e8415b922150643b866098e61c
mariaarnesson <maria_arnesson@outlook.com>
2023-01-28 20:30:37 +0000

    Initial commit

--- reservation/urls.py
+++ reservation/urls.py
@@ -1,8 +1,6 @@
-from . import views
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='index'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+    path('', views.index, name='index'),
 ]
